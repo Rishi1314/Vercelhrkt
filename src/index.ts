9179328ab91cb8a3e9578e7bab43fbe704674c9f
Rishi1314 <rishiraj13052003@gmail.com>
2025-01-25 13:36:47 -0500

    Created Cloning Repo Function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import cors from "cors";
+import { generate } from "./util";
+import simpleGit from "simple-git";
 
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
+  // console.log(req.body);
+  
     const repoUrl=req.body.repoUrl;
-    console.log(repoUrl);
-    res.json({message: "Deploying the repository"});
+    const id = generate();
+    await simpleGit().clone(repoUrl, `output/${id}`);
+    res.json({id:id,message: "Deploying the repository"});
 })
 
 app.listen(3000, () => {
